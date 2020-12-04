d958e71b9d73f8d42affb0ded2b4a6762a30382b
DeBagBot <debagbot@outlook.com>
2020-12-04 21:07:06 +0300

    Test packages commit

---
+++ ConsoleCarCrew/src/test/java/ConsoleCarCrew/ConsoleCarCrewSuite.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConsoleCarCrew;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author DeBagBot
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ConsoleCarCrew.MainTest.class})
+public class ConsoleCarCrewSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
