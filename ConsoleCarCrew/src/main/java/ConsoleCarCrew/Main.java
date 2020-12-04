07076f05ff329a68d1bb8ff313b1f838ca76f09a
DeBagBot <debagbot@outlook.com>
2020-12-04 21:04:18 +0300

    Future branch initial commit

--- ConsoleCarCrew/src/main/java/ConsoleCarCrew/Main.java
+++ ConsoleCarCrew/src/main/java/ConsoleCarCrew/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("Start");
+        System.out.println("Start future branch");
     }
     
 }
